60a75867e5aad3004e1b0a3e74003fa84f58428b
Lans12345 <olanalans12345@gmail.com>
2025-11-18 10:51:59 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:buddyapp/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:buddyapp/utils/app_theme.dart';
@@ -6,7 +8,10 @@ import 'package:buddyapp/screens/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp(
+    name: 'shopbuddy-e017c',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   // Set preferred orientations
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
